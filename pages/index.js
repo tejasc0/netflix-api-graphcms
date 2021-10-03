8ef1ec3017d4fc190fd5899ecd7e46fb9da25037
tejasc0 <tejasc888@gmail.com>
2021-10-03 16:34:18 +0000

    added rich text functionality

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { gql, GraphQLClient } from 'graphql-request'
 import Section from '../components/Section'
+import { RichText } from '@graphcms/rich-text-react-renderer';
+import React from 'react';
 
 export const getStaticProps = async () => {
   const url = process.env.ENDPOINT_URL
@@ -37,20 +39,33 @@ account(where: { id: "cktmn8jbs693z0a81u2vhb5kl"}) {
 }
 `
 
+const richtextQuery = gql`
+query {
+ para(where: { slug: "twjkt4i"}) {
+  paragr2 {
+    raw
+  }
+}
+}
+`
+
 const data = await graphQLClient.request(videoQuery) 
 const videos = data.videos
 const accountData = await graphQLClient.request(accountQuery)
 const account = accountData.account
+const richtextData = await graphQLClient.request(richtextQuery)
+const richtext = richtextData.para
 
 return { 
   props: {
     videos,
-    account
+    account,
+    richtext
 }
 }
 }
 
-export default function Home({ videos, account }) {
+export default function Home({ videos, account, richtext }) {
   const randomVideo = (videos) => {
     return videos = videos[Math.floor(Math.random() * videos.length)]
 
@@ -72,6 +87,12 @@ const unSeenVideos = (videos) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
+      <RichText
+        content={richtext.paragr2.raw}
+        renderers={{
+        p: ({ children }) => <div className="text-green-700 2 my-3 mx-16 text-md">{children}</div>,
+        }}
+      />
         <div><p>Welcome <span>{account.username}</span></p></div>
         <div>
           <img src={randomVideo(videos).thumbnail.url} alt={randomVideo(videos).thumbnail.title} />
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  textIndent: {
+    '1': '0.25rem',
+    '2': '0.5rem',
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
